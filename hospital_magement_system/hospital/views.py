28b1548e0d2ac4c89cdf2badaa6b549283c29d1c
Natnael-Negash <nnegash658@gmail.com>
2024-07-09 04:25:52 -0900

    views

--- hospital_magement_system/hospital/views.py
+++ hospital_magement_system/hospital/views.py
@@ -28,24 +28,22 @@ def doctorbutton_view(request):
 
 #showing login/signup button for patient
 
-def patientbuttom_view(request):
+def patientbutton_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     return render(request,'hospital/patientclick.html')
 
 
 def admin_signup_view(request):
-    form= forms.AdminsignupForm()
-    if request.method == 'POST':
-        form=forms.AdminSignupForm(request.POST)
-        if form.is_valid():
-            user=form.save()
-            user.set_password(user.password)
-            user.save()
-            my_admin_group = Group.objects.get_or_create(name='ADMIN')
-            my_admin_group[0].user_set.add(user)
-            return HttpResponseRedirect('adminlogin')
-    return render(request, 'hospital/adminsignup.html',{'form':form})
+    form = forms.AdminSignupForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        Group.objects.get_or_create(name='ADMIN')[0].user_set.add(user)
+        return HttpResponseRedirect('adminlogin')
+    return render(request, 'hospital/adminsignup.html', {'form': form})
+
 
 def doctor_signup_view(request):
     userForm=forms.DoctorUserForm()
